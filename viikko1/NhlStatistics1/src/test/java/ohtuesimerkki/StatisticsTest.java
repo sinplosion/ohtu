1c5354f365f2e80d8174ddba7f0303eb534995f5
sinplosion <ziggy.starzacher@gmail.com>
2020-03-27 23:09:34 +0200

    viikko 1 partially

---
+++ viikko1/NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -0,0 +1,14 @@
+package ohtuesimerkki;
+
+import org.junit.Before;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
