71261a08a689914a70292c9278c5a1bcaee95b61
sinplosion <ziggy.starzacher@gmail.com>
2020-03-29 02:59:03 +0200

    viikko 2 teht

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/KirjanpitoInterface.java
@@ -0,0 +1,10 @@
+package ohtu.verkkokauppa;
+
+import java.util.ArrayList;
+
+public interface KirjanpitoInterface {
+
+    ArrayList<String> getTapahtumat();
+
+    void lisaaTapahtuma(String tapahtuma);
+}
